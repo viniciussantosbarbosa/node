338cbbf2f8044575f4ef3c0f916a36ef954ee9f0
= <=>
2022-11-01 22:02:01 -0300

    Fundamentos da linguagem JS

---
+++ calculoimc.js
@@ -0,0 +1,9 @@
+const read = require('readline-sync')
+
+
+console.log ("===== CALCULO IMC =====")
+
+let peso = Number(read.question("Digite a nota1: ").replace(",",","))
+let altura = Number(read.question("Digite a nota2: ").replace(",",","))
+
+let 
---
+++ porcentagem.js
@@ -0,0 +1,8 @@
+//Cálculo de porcentagem
+
+
+//importar pacote
+const read = require('readline-sync')
+
+console.log ("===== Calculo porcentagem =====")
+let nome = read.question()
