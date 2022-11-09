a9c4296f1021f9292b50bb689fb712b05f8f4fdd
= <=>
2022-11-08 21:50:48 -0300

    Exercicios js no code

---
+++ arrayMultidimensional.js
@@ -0,0 +1,13 @@
+/**
+ * Estudo do array Multidimensional - Matriz
+ */
+
+//[]linha 1
+// []linha 2
+
+//            Matemática Português
+let boletim = [[8,7,9,3],[4,5,8,6]]
+//recuperando um valor da matrix
+//Recuperar a nota de matemática do 2*bimestre
+//[linha] [coluna]
+console.log(`Nota: ${boletim[0][1]}`)
