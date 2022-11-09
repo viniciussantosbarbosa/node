99a46fc864f7350a9a0e43f9bc504e69c46d51eb
= <=>
2022-11-08 21:53:30 -0300

    booleanos

--- arrayMultidimensional.js
+++ arrayMultidimensional.js
@@ -9,5 +9,8 @@
 let boletim = [[8,7,9,3],[4,5,8,6]]
 //recuperando um valor da matrix
 //Recuperar a nota de matemática do 2*bimestre
-//[linha] [coluna]
+//[linha] []
 console.log(`Nota: ${boletim[0][1]}`)
+
+//Recuperar a nota de Português do 3*bimestre
+console.log(`Nota: ${boletim[1][2]}`)
